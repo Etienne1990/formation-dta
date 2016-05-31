ea68745d69b3b57f14c2d5b3669de29fa0c7a8ad
Etienne Bohain <EtienneTiti1990@gmail.com>
2016-05-31 17:31:18 +0200

    Utilisation des annotations au lieu des xml.

--- pizzeria-console-objet-java8-maven-multimodule/pizzeria-console-objet-java8/src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-console-objet-java8-maven-multimodule/pizzeria-console-objet-java8/src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,13 +1,11 @@
 package fr.pizzeria.console;
 
 import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import fr.pizzeria.dao.pizza.PizzaDaoImpl;
 import fr.pizzeria.ihm.menu.Menu;
@@ -16,10 +14,6 @@ import fr.pizzeria.ihm.menu.Menu;
  * Classe principale de l'application.
  */
 public class PizzeriaAdminConsoleApp {
-	private static final String APPLICATION_CONFIG_XML = "application-config.xml";
-	private static final String FILE_APLLICATION_PROP = "application";
-	private static final String PROPERTY_DAO_IMPL = "dao.impl";
-
 	private PizzeriaAdminConsoleApp() {}
 
 	/**
@@ -30,17 +24,9 @@ public class PizzeriaAdminConsoleApp {
 	public static void main(String[] args) {
 		Logger.getLogger("org").setLevel(Level.SEVERE);
 		Locale.setDefault(Locale.FRENCH);
-		try {
-			ResourceBundle bundle = ResourceBundle.getBundle(FILE_APLLICATION_PROP);
-			String fileDaoImpl = bundle.getString(PROPERTY_DAO_IMPL);
-			System.out.println("DAO : " + fileDaoImpl);
-			try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(APPLICATION_CONFIG_XML, fileDaoImpl)) {
-				Menu menu = context.getBean(Menu.class);
-				menu.afficher();
-			}
-		} catch (MissingResourceException e) {
-			System.err.println(
-					"Erreur: Le fichier " + FILE_APLLICATION_PROP + ".properties doit contenir la propriété \"" + PROPERTY_DAO_IMPL + "\" désignant le fichier de configuration Spring pour la DAO.");
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PizzeriaAppSpringConfig.class)) {
+			Menu menu = context.getBean(Menu.class);
+			menu.afficher();
 		}
 	}
 }
